67671b76a7f6b94b3e832d572fd51b4cf976020d
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-01 20:31:00 +0000

    added time1.js

---
+++ timer1.js
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2)
+//ensure argument is given
+if (args.length === 0) {
+  console.log('No alarms given')
+} else {
+
+  args.forEach(ele => {
+    setTimeout(function() {
+      //edge cases, negatives, Nan, blanks
+      if (ele < 0 || isNaN(ele)===true || ele === "") {
+        
+      } else {
+        //otherwise execute alarm at given time
+        console.log(`This alarm goes of at ${ele} seconds`);
+        process.stdout.write('\x07');
+      }
+    }, ele*1000)
+  });
+
+}
